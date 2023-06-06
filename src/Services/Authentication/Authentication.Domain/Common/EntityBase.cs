437a5d6aa4f35ad03b58bd97da823d4246846c36
Bilal Ur Rahman <bilal.ur.rahman@hotmail.com>
2023-06-06 19:51:26 +0300

    --Implementation started for Register user

---
+++ src/Services/Authentication/Authentication.Domain/Common/EntityBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Authentication.Domain.Common
+{
+    public abstract class EntityBase
+    {
+        public int Id { get; protected set; }
+        public string CreatedBy { get; set; }
+        public string LastModifiedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime LastModifiedDate { get; set; }
+        public bool isActive { get; set; } = true;
+
+    }
+}
