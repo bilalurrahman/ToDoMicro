65573b6f9b0b7a2e5620b55a7ec66b3e6606ee80
Bilal Ur Rahman <bilal.ur.rahman@hotmail.com>
2023-06-19 10:45:00 +0300

    Fixes on injection

--- src/Services/Tasks/Tasks.API/InjectionExtension.cs
+++ src/Services/Tasks/Tasks.API/InjectionExtension.cs
@@ -2,6 +2,8 @@
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
 using Tasks.Application.Contracts;
+using Tasks.Application.Contracts.Context;
+using Tasks.Infrastructure.Context;
 using Tasks.Infrastructure.Persistance;
 
 namespace Tasks.API
@@ -18,6 +20,7 @@ namespace Tasks.API
         public static IServiceCollection AddDependencies(this IServiceCollection services)
         {
             services.AddScoped<ITasksCommandsRepository, TasksCommandRepository>();
+            services.AddScoped<ITasksContext, TasksContext>();
            
             return services;
         }
