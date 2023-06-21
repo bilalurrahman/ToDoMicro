7a99db09a22a2ea2adeed861da4a79faf70f927e
Bilal Ur Rahman <bilal.ur.rahman@hotmail.com>
2023-06-21 07:22:44 +0300

    --emailnotification implemented

--- src/Services/Announcement/Annoucement.Integration/Integration/EmailIntegration.cs
+++ src/Services/Announcement/Annoucement.Integration/Integration/EmailIntegration.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using Announcement.Application.Contracts.Integration;
+using Announcement.Application.Models;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.Extensions.Options;
+using MimeKit;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +12,30 @@ using System.Threading.Tasks;
 
 namespace Annoucement.Infrastructure.Integration
 {
-    public class EmailIntegration
-    {
+    
 
+    public class EmailIntegration : IEmailIntegration
+    {
+        private readonly MailSettings _mailSettings;
+        public EmailIntegration(IOptions<MailSettings> mailSettings)
+        {
+            _mailSettings = mailSettings.Value;
+        }
+
+        public async Task SendEmailAsync(MailRequest mailRequest)
+        {
+            var email = new MimeMessage();
+            email.Sender = MailboxAddress.Parse(_mailSettings.Mail);
+            email.To.Add(MailboxAddress.Parse(mailRequest.ToEmail));
+            email.Subject = mailRequest.Subject;
+            var builder = new BodyBuilder();
+            builder.HtmlBody = mailRequest.Body;
+            email.Body = builder.ToMessageBody();
+            using var smtp = new SmtpClient();
+            smtp.Connect(_mailSettings.Host, _mailSettings.Port, SecureSocketOptions.StartTls);
+            smtp.Authenticate(_mailSettings.Mail, _mailSettings.Password);
+            await smtp.SendAsync(email);
+            smtp.Disconnect(true);
+        }
     }
 }
--- src/Services/Announcement/Announcement.Application/Contracts/Integration/IEmailIntegration.cs
+++ src/Services/Announcement/Announcement.Application/Contracts/Integration/IEmailIntegration.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Announcement.Application.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,9 @@ using System.Threading.Tasks;
 
 namespace Announcement.Application.Contracts.Integration
 {
-    public class IEmailIntegration
+    public interface IEmailIntegration
     {
+        Task SendEmailAsync(MailRequest mailRequest);
+
     }
 }
