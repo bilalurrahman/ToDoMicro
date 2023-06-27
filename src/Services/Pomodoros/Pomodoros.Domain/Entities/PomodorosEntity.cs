2b6f9bca46a898df3aeca17d5dbfd9fc0ad57276
Bilal Ur Rahman <bilal.ur.rahman@hotmail.com>
2023-06-27 16:42:04 +0300

    -Pomodoro Structure Implementations

---
+++ src/Services/Pomodoros/Pomodoros.Domain/Entities/PomodorosEntity.cs
@@ -0,0 +1,17 @@
+﻿using Pomodoros.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pomodoros.Domain.Entities
+{
+    public class PomodorosEntity :EntityBase
+    {
+        public string TaskId { get; set; }
+        public TimeSpan StartTime { get; set; }
+        public TimeSpan EndTime { get; set; }
+        public int Duration { get; set; }
+    }
+}
---
+++ src/Services/Pomodoros/Pomodoros.Infrastructure/Persistance/Command/CommandPomodorosRepository.cs
@@ -0,0 +1,37 @@
+﻿using MongoDB.Driver;
+using Pomodoros.Application.Contracts.Context;
+using Pomodoros.Application.Contracts.Persistance.Command;
+using Pomodoros.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pomodoros.Infrastructure.Persistance.Command
+{
+    public class CommandPomodorosRepository : ICommandPomodorosRepository
+    {
+        private readonly IPomodoroContext _context;
+
+        public CommandPomodorosRepository(IPomodoroContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Add(PomodorosEntity entity)
+        {
+            await _context.PomodorosCollection.InsertOneAsync(entity);
+        }
+
+        public async Task<bool> Update(PomodorosEntity entity)
+        {
+            var updateResult = await _context
+                .PomodorosCollection
+                .ReplaceOneAsync(filter: g => g.Id == entity.Id, replacement: entity);
+
+            return updateResult.IsAcknowledged
+                && updateResult.ModifiedCount > 0;
+        }
+    }
+}
